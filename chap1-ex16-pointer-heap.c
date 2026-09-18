f71a683746bc332e18c408fb5edd61f1c13eb0d5
Ahmad Nijad <ahmadnijadstudy@gmail.com>
2026-09-18 16:16:49 +0800

    Rename file and update comments for clarity

---
+++ chap1-ex16-pointer-heap.c
@@ -0,0 +1,19 @@
+//Example 16
+//pointer using dynamic memory allocation or heap
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* create_an_integer(int default_value) {
+  int* var_ptr = (int*)malloc(sizeof(int));
+  *var_ptr = default_value;
+  return var_ptr;
+}
+
+int main() {
+  int* ptr = NULL;
+  ptr = create_an_integer(10);
+  printf("%d\n", *ptr);
+  free(ptr);
+  return 0;
+}
